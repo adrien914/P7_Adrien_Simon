34726c424003d5fe68db9b71f4553f0b591f1661
adrien914 <Tupuduku.91322>
2020-09-23 16:09:08 +0200

    requests instead of wikipedia module

--- utils/google_maps_api.py
+++ utils/google_maps_api.py
@@ -1,5 +1,4 @@
 from decouple import config
-import googlemaps
 import requests
 
 
--- utils/wikipedia.py
+++ utils/wikipedia.py
@@ -1,14 +1,36 @@
-import wikipedia
-
+import requests
 
 class Wikipedia:
     """Class used for the media wiki API REST requests"""
 
     @staticmethod
     def get_request(sentence: str) -> str:
-        wikipedia.set_lang("fr")
+        url = "https://fr.wikipedia.org/w/api.php"
+        parameters = {
+            "action": "query",
+            "list": "search",
+            "format": "json",
+            "utf8": True,
+            "srsearch": sentence,
+        }
+        response = requests.get(url, parameters).json()
+        if not response["query"]["searchinfo"]["totalhits"]:
+            return ""
+        page = response["query"]["search"][0]
+        page_id = page["pageid"]
+        parameters = {
+            "action": "query",
+            "format": "json",
+            "prop": "extracts",
+            "exintro": True,
+            "explaintext": True,
+            "redirects": 1,
+            "exsentences": 2,
+            "pageids": page_id,
+        }
+        response = requests.get(url, parameters).json()
         try:
-            result = wikipedia.summary(sentence, sentences=2)
+            summary = response["query"]["pages"][str(page_id)]["extract"]
+            return summary
         except:
-            result = None
-        return result
+            return ""
