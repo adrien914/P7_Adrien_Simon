e2948f7e9edd7409311c95160696f0730842e21e
adrien914 <Tupuduku.91322>
2020-09-25 16:12:14 +0200

    pep8

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -16,12 +16,14 @@ def test_wikipedia(monkeypatch):
            }],
           "pages": {
              "16": {
-                "extract":"L\u2019arc de triomphe de l\u2019\u00c9toile, souvent appel\u00e9 simplement l\u2019Arc de triomphe, dont la construction, d\u00e9cid\u00e9e par l'empereur Napol\u00e9on Ier, d\u00e9buta en 1806 et s'acheva en 1836 sous Louis-Philippe, est situ\u00e9 \u00e0 Paris, dans les 8e, 16e, et 17e arrondissements."
+                "extract":"L\u2019arc de triomphe de l\u2019\u00c9toile, souvent appel\u00e9 simplement l\u2019"
+                          "Arc de triomphe, dont la construction, d\u00e9cid\u00e9e par l'empereur Napol\u00e9on Ier, "
+                          "d\u00e9buta en 1806 et s'acheva en 1836 sous Louis-Philippe, est situ\u00e9 \u00e0 Paris, "
+                          "dans les 8e, 16e, et 17e arrondissements."
              }
           }
        }
     }
-
 
     class MockResponse:
         def __init__(self, url, params):
